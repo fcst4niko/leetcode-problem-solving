a25ef1b870a5b4fd9dbc33cd693a645b5c1f8327
Fernando Cataldo <fernando.cataldos@gmail.com>
2025-02-05 12:13:15 -0300

    Leetcode exercise 55, done! TC: O(N)

---
+++ leetcode-problem-solving/121. Best Time to Buy and Sell Stock/Program.cs
@@ -0,0 +1,15 @@
+﻿//// See https://aka.ms/new-console-template for more information
+
+//using leetcode_problem_solving._122._Best_Time_to_Buy_and_Sell_Stock_II;
+//using System.Diagnostics;
+
+//Solution Solution = new Solution();
+
+////int[] prices = { 7, 1, 5, 3, 6, 4 };
+//int[] prices = { 1, 2, 3, 4, 5 };
+////int[] prices = { 7, 6, 4, 3, 1 };
+
+
+//Solution.MaxProfit(prices);
+
+//Console.ReadLine();
---
+++ leetcode-problem-solving/55. Jump Game/Solution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_problem_solving._55._Jump_Game
+{
+    public class Solution
+    {
+        public bool CanJump(int[] nums)
+        {
+            if (nums.Length == 0) return false;
+            if (nums[0] == 0 && nums.Length == 1) return true;
+            if (nums[0] == 0 && nums.Length > 1) return false;
+
+            for (int i = nums.Length - 2; i >= 0; i--)
+            {
+                if (nums[i] == 0)
+                {
+                    int cont = 1;
+                    bool result = false;
+                    for (int j = i - 1; j >= 0; j--)
+                    {
+                        if (nums[j] > cont)
+                        {
+                            result = true;
+                            break;
+                        }
+                        cont++;
+                    }
+
+                    if (result == false) return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
