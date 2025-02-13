ec3555a5e7a0d48b1a63b34e8dc56732de3d6959
Fernando Cataldo <fernando.cataldos@gmail.com>
2025-02-13 15:07:09 -0300

    Leetcode exercise 380, done! TC O(1)

---
+++ leetcode-problem-solving/380. Insert Delete GetRandom O(1)/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_problem_solving._380._Insert_Delete_GetRandom_O_1_
+{
+    public class RandomizedSet
+    {
+        private readonly HashSet<int> set;
+
+        public RandomizedSet()
+        {
+            set = new HashSet<int>();
+        }
+
+        public bool Insert(int val)
+        {
+            return set.Add(val);
+        }
+
+        public bool Remove(int val)
+        {
+            return set.Remove(val);
+        }
+
+        public int GetRandom()
+        {
+            if (!set.Any()) return 0;
+
+            var values = set.ToArray();
+            
+            Random random = new Random();
+
+            // Generamos un índice aleatorio entre 0 y el número de elementos - 1
+            int indiceAleatorio = random.Next(values.Count());
+
+            // Obtenemos el número aleatorio de la colección
+            return values[indiceAleatorio];
+        }
+    }
+}
--- leetcode-problem-solving/obj/Debug/net8.0/leetcode-problem-solving.AssemblyInfo.cs
+++ leetcode-problem-solving/obj/Debug/net8.0/leetcode-problem-solving.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("leetcode-problem-solving")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ffe45c5d62a588f4b84caf8d1698759410059f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29b210156afe01711b7f1f0cd9d5b953b6867979")]
 [assembly: System.Reflection.AssemblyProductAttribute("leetcode-problem-solving")]
 [assembly: System.Reflection.AssemblyTitleAttribute("leetcode-problem-solving")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
