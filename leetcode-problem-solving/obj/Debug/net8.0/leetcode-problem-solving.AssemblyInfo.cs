95d4b2e526000bd046d408a892c89514a02a5de3
Fernando Cataldo <fernando.cataldos@gmail.com>
2025-02-13 15:07:33 -0300

    Remaining

--- leetcode-problem-solving/obj/Debug/net8.0/leetcode-problem-solving.AssemblyInfo.cs
+++ leetcode-problem-solving/obj/Debug/net8.0/leetcode-problem-solving.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("leetcode-problem-solving")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29b210156afe01711b7f1f0cd9d5b953b6867979")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec3555a5e7a0d48b1a63b34e8dc56732de3d6959")]
 [assembly: System.Reflection.AssemblyProductAttribute("leetcode-problem-solving")]
 [assembly: System.Reflection.AssemblyTitleAttribute("leetcode-problem-solving")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
