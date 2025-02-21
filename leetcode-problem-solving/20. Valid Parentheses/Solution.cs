3bd12b38702cd467656aa52113644451a9b11aa8
Fernando Cataldo <fernando.cataldos@gmail.com>
2025-02-21 15:51:09 -0300

    Leetcode exercise 20, done TC O(N)

---
+++ leetcode-problem-solving/20. Valid Parentheses/Solution.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+
+namespace leetcode_problem_solving._20._Valid_Parentheses
+{
+    public class Solution
+    {
+        public bool IsValid(string s)
+        {
+            if (s.Length < 1 || s.Length % 2 != 0 ) return false;
+
+            var bracketStack = new Stack<char>();
+
+            var bracketDictionary = new Dictionary<char, char>
+            {
+                { '(', ')' },
+                { '{', '}' },
+                { '[', ']' }
+            };
+
+            var i = 0;
+            while (i < s.Length)
+            {
+                var currentChar = s[i];
+                if (bracketDictionary.ContainsKey(currentChar))
+                {
+                    bracketStack.Push(currentChar);
+                }
+                else if (bracketStack.Any())
+                {
+                    var lastChar = bracketStack.First();
+                    var lastClosingBracket = bracketDictionary[lastChar];
+
+                    if (lastClosingBracket != currentChar)
+                    {
+                        return false;
+                    }
+                    bracketStack.Pop();
+                }
+                else
+                {
+                    return false;
+                }
+
+                i++;
+            }
+
+            return !bracketStack.Any();
+        }
+    }
+}
---
+++ leetcode-problem-solving/56. Merge Intervals/Program.cs
@@ -0,0 +1,30 @@
+﻿//// See https://aka.ms/new-console-template for more information
+
+//using leetcode_problem_solving._56._Merge_Intervals;
+
+//Solution solution = new Solution();
+
+////int[][] intervals = [[1, 3], [2, 6], [8, 10], [15, 18]];
+////int[][] intervals = [[1, 4], [0, 4]];
+////int[][] intervals = [[1, 4], [0, 1]];
+////int[][] intervals = [[1, 4], [4, 5]];
+////int[][] intervals = [[1, 4], [0, 0]];
+////int[][] intervals = [[1, 4], [0, 2], [3, 5]];
+//int[][] intervals = [[2, 3], [4, 5], [6, 7], [8, 9], [1, 10]];
+
+//var mergedIntervals = solution.Merge(intervals);
+
+//// [1, 3]
+//// [2, 6]
+//// [8, 10][15, 18]
+
+//for (int i = 0; i < mergedIntervals.Length; i++)
+//{
+//	for (int j = 0; j < mergedIntervals[i].Length; j++)
+//	{
+//		Console.WriteLine(mergedIntervals[i][j]); 
+//	}
+//}
+
+
+//Console.ReadLine();
