c0aa74dd70cd97e71c3046bcb764ee524830537f
Fernando Cataldo <fernando.cataldos@gmail.com>
2025-03-05 16:27:05 -0300

    Avances: 48. Rotate Image

--- leetcode-problem-solving/Program.cs
+++ leetcode-problem-solving/Program.cs
@@ -1,18 +1,21 @@
-﻿//// See https://aka.ms/new-console-template for more information
+﻿// See https://aka.ms/new-console-template for more information
 
-//using leetcode_problem_solving._238._Product_of_Array_Except_Self;
+using leetcode_problem_solving._48._Rotate_Image;
 
-//Solution solution = new Solution();
-
-////int[] nums = { 1, 2, 3, 4 };
-//int[] nums = { -1, 1, 0, -3, 3 };
+Solution solution = new Solution();
 
 
-//var result = solution.ProductExceptSelf(nums);
+int[][] matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
 
-//foreach (var item in result)
-//{
-//    Console.WriteLine(item);
-//}
 
-//Console.ReadLine();
+solution.Rotate(matrix);
+
+for (int i = 0; i < matrix.Length; i++)
+{
+	for (int j = 0; j < matrix[0].Length; j++)
+	{
+		Console.WriteLine(matrix[j][i]);
+	}
+}
+
+Console.ReadLine();
--- leetcode-problem-solving/obj/Debug/net8.0/leetcode-problem-solving.AssemblyInfo.cs
+++ leetcode-problem-solving/obj/Debug/net8.0/leetcode-problem-solving.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("leetcode-problem-solving")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83a1a34a1ed597a73bf7d7233f06952123a4668d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98fb11b768f391e2bf18760715e6bce2953799da")]
 [assembly: System.Reflection.AssemblyProductAttribute("leetcode-problem-solving")]
 [assembly: System.Reflection.AssemblyTitleAttribute("leetcode-problem-solving")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
