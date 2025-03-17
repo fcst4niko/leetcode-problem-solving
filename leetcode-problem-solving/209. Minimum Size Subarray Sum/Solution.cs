b89309243f5df8703aaa62f9367561f9c6964199
Fernando Cataldo <fernando.cataldos@gmail.com>
2025-03-16 22:56:14 -0300

    Avances: 209. Minimum Size Subarray Sum

--- leetcode-problem-solving/209. Minimum Size Subarray Sum/Solution.cs
+++ leetcode-problem-solving/209. Minimum Size Subarray Sum/Solution.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,22 +13,38 @@ namespace leetcode_problem_solving._209._Minimum_Size_Subarray_Sum
         {
             if (nums.Contains(target)) return 1;
 
-            int window = 1;
-
+            int window = 0;
             int sum = 0;
+            bool isBreaked = false;
 
-            while (window <= nums.Length)
+            //buscamos primero la máxima ventana que sirve para luego ir reduciendo la ventana...
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+                window++;
+                if (sum >= target)
+                {
+                    isBreaked = true;
+                    break;
+                }
+            }
+
+            if (!isBreaked)
+            {
+                return 0;
+            }
+
+            sum = 0;
+            var prevWindow = window;
+            window--;
+            while (window > 0)
             {
                 for (int i = 0; i < window; i++)
                 {
                     sum += nums[i];
                 }
 
-                if (sum >= target)
-                {
-                    return window;
-                }
-                else
+                if (sum < target)
                 {
                     for (int i = window; i < nums.Length; i++)
                     {
@@ -35,13 +52,21 @@ namespace leetcode_problem_solving._209._Minimum_Size_Subarray_Sum
 
                         if (sum >= target)
                         {
-                            return window;
+                            isBreaked = true;
+                            break;
                         }
                     }
 
-                    window++;
-                    sum = 0;                    
+                    if (!isBreaked)
+                    {
+                        return prevWindow;
+                    }                    
                 }
+
+                isBreaked = false;
+                prevWindow = window;
+                window--;
+                sum = 0;
             }
 
             return 0;
